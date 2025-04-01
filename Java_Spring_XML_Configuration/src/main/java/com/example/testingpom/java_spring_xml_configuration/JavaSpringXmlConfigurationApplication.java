9d44908b619479450c6980e84943e497dfb394db
Amayuru Viduranga <100108274+VidurangaDev@users.noreply.github.com>
2025-04-01 12:56:50 +0530

    project created

---
+++ Java_Spring_XML_Configuration/src/main/java/com/example/testingpom/java_spring_xml_configuration/JavaSpringXmlConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingpom.java_spring_xml_configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringXmlConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringXmlConfigurationApplication.class, args);
+    }
+
+}
---
+++ Java_Spring_XML_Configuration/src/test/java/com/example/testingpom/java_spring_xml_configuration/JavaSpringXmlConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingpom.java_spring_xml_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringXmlConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
